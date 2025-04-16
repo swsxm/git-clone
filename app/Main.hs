a3979753e4a37bb352657ddd2365b31718707b99
swsxm <129607562+swsxm@users.noreply.github.com>
2025-04-16 23:39:34 +0200

    add: url parsing

--- app/Main.hs
+++ app/Main.hs
@@ -16,6 +16,34 @@ import Data.Word8 (isSpace)
 
 newtype PacketLine = PacketLine B.ByteString deriving (Show)
 
+data Url = Url {
+    protocol :: String,
+    address  :: String,
+    name     :: String
+} deriving (Show, Eq)
+
+splitOn :: String -> Char -> [String]
+splitOn str delim = go str "" []
+  where
+    go [] accPart accList = accList ++ [accPart]  
+    go (x:xs) accPart accList
+      | x == delim = go xs "" (accList ++ [accPart])  
+      | otherwise  = go xs (accPart ++ [x]) accList
+
+
+parseString :: String -> Url
+parseString url = 
+  let parts = foldr removeEmpty [] (splitOn url '/')
+  in case parts of
+    [protoStr, addrStr, nameStr] ->
+      let proto = takeWhile (/= ':') protoStr
+      in Url proto addrStr nameStr
+    _ -> error "invalid url"
+  where
+    removeEmpty str acc
+      | str == ""  = acc
+      | otherwise  = str : acc
+
 withConnection :: HostName -> ServiceName -> (Socket -> IO b) -> IO b
 withConnection host port consumer = do
     sock <- openConnection host port
